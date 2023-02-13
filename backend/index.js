a47246756ce1bf3606f2e484d9d6e84e9507601f
Abhishek Sukumar <abhishekskmr44@gmail.com>
2023-02-13 19:02:18 +0530

    Agumentiki

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 const SocialLinkSchema = require("./models/Social.Schema");
 const cors = require("cors");
-const EducationTypeModel = require("./models/EducationType.schema");
+const educationTypeModel = require("./models/educationType.schema");
 const PORT = process.env.PORT || 8080;
 app.use(express.json());
 app.use(cors())
@@ -36,19 +36,19 @@ app.get("/social", async (req, res) => {
   }
 });
 
-app.post("/Education", async (req, res) => {
+app.post("/education", async (req, res) => {
   const { title, text } = req.body;
   try {
-    let Education = new EducationTypeModel({ title, text });
-    await Education.save();
-    res.status(404).send(Education);
+    let education = new educationTypeModel({ title, text });
+    await education.save();
+    res.status(404).send(education);
   } catch (e) {
     res.status(500).send(e);
   }
 });
-app.get("/Education", async (req, res) => {
+app.get("/education", async (req, res) => {
   try {
-    let links = await EducationTypeModel.find({});
+    let links = await educationTypeModel.find({});
     res.status(200).send(links);
   } catch (e) {
     res.status(500).send(e.message);
--- backend/models/JobType.schema.js
+++ backend/models/JobType.schema.js
@@ -1,11 +1,11 @@
 const {Schema,model}=require("mongoose");
 
 
-const EducationTypeSchema=Schema({
+const educationTypeSchema=Schema({
     title:{type:String},
     text:{type:String}
 
 })
 
-const EducationTypeModel=model("Education",EducationTypeSchema)
-module.exports=EducationTypeModel
+const educationTypeModel=model("education",educationTypeSchema)
+module.exports=educationTypeModel
